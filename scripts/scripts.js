40cb32325dd306660ae8cde306844b8b640e81bd
Isaac <59330021+bytesandroses@users.noreply.github.com>
2024-05-29 19:37:11 +0200

    Reduce the width of color palette

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,6 @@
-// import iro module/library package
-import iro from '@jaames/iro';
+// import iro from '@jaames/iro';
 
-const colorPicker = new iro.ColorPicker("#picker");
+const colorPicker = new iro.ColorPicker("#picker", {
+  width: 150,
+  color: "#f00"
+});
