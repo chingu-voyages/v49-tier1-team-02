c701b20e01ee2fe785df4e71e130a372a24919db
EslamAbdElhakem <eslam.hakem12@gmail.com>
2024-05-30 17:31:47 +0300

    Refactor HTML structure and remove unused code
    Summary
    color picker need styling and changes and comply with project wireframe
    
    HTML Changes:
    
    Added a script tags to the end of HTML Files "best practices"
    Added main tag to contain the color palate "semantic HTML"
    Remove hsl code to comply with project wireframe
    
    CSS Changes:
    
    style color palate using flex to center it in middle of webpage.
    
    JavaScript Changes:
    
    Remove hsl related code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,11 +5,9 @@ const colorPicker = new iro.ColorPicker("#picker", {
 
 const hexCode = document.querySelector("#hex-code");
 const rgbCode = document.querySelector("#rgb-code");
-const hslCode = document.querySelector("#hsl-code");
 
 colorPicker.on(["color:init", "color:change"], function 
 (color) {
   hexCode.innerHTML = ("HEX Code: " + color.hexString);
   rgbCode.innerHTML = ("RGB Code: " + color.rgbString);
-  hslCode.innerHTML = ("HSL Code: " + color.hslString);
 });
