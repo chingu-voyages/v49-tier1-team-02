d9e9b3ad7321af67788c9c2498c63b17e82f3a71
Isaac <59330021+bytesandroses@users.noreply.github.com>
2024-05-31 08:45:41 +0200

    Add selector so that hex color code can be manipulated in DOM

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,11 @@
+const hexCode = document.querySelector("#hex-code");
+const rgbCode = document.querySelector("#rgb-code");
+const hexInput = document.querySelector("#hexInput");
+
 const colorPicker = new iro.ColorPicker("#picker", {
   width: 150,
   color: "#f00"
 });
-
-const hexCode = document.querySelector("#hex-code");
-const rgbCode = document.querySelector("#rgb-code");
 
 colorPicker.on(["color:init", "color:change"], function 
 (color) {
