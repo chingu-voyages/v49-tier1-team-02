cca2821d4447de85994544aef08522b370b907c5
EslamAbdElhakem <eslam.hakem12@gmail.com>
2024-06-03 23:41:13 +0300

    Add header, main content, and footer sections to index.html*** ***Update color picker functionality in scripts.js*** ***Update styles for main section and color list in styles.css

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,26 +1,62 @@
 /* create a new color picker instance */
 const colorPicker = new iro.ColorPicker("#picker", {
   width: 150,
-  color: "#f00"
+  colors: ["#f00",
+          "#ff0",
+          "#0f0",
+          "#0ff",
+          "#00f",
+          "#f0f",
+          "#f00"
+] 
 });
+
+const colorList = document.getElementById("colorList");
+const activeColor = document.getElementById("activeColor");
+
+function setColor(colorIndex) {
+  // setActiveColor expects the color index!
+  colorPicker.setActiveColor(colorIndex);
+}
 
 /* get the color picker's color */
 const hexCode = document.querySelector("#hex-code");
 const rgbCode = document.querySelector("#rgb-code");
 const hexInput = document.querySelector("#hexInput");
-const colorPreview = document.querySelector("#color-preview-box");
 
 /* set the color picker's color */
-colorPicker.on(["color:init", "color:change"], function 
-(color) {
-  hexCode.innerHTML = ("HEX Code: " + color.hexString);
-  rgbCode.innerHTML = ("RGB Code: " + color.rgbString);
-  hexInput.value = color.hexString;
-  colorPreview.style.backgroundColor = color.hexString;
+colorPicker.on(["color:init", "color:change", "color:mount"], function 
+(colors) {
+  hexCode.innerHTML = ("HEX Code: " + colors.hexString);
+  rgbCode.innerHTML = ("RGB Code: " + colors.rgbString);
+  colorList.innerHTML = '';
+  hexInput.value = colors.hexString;
+  
+  
+  colorPicker.colors.forEach(colors => {
+    const index = colors.index;
+    const hexString = colors.hexString;
+    colorList.innerHTML += `
+      <li onClick="setColor(${ index })">
+        <div class="swatch" style="background: ${ hexString }"></div>
+        <span>${ index }: ${ hexString }</span>
+      </li>
+    `;
+  });
 });
 
 /* change the color picker's color */
 hexInput.addEventListener('change', function
-(color) {
-  colorPicker.color.hexString = this.value;
+(colors) {
+  colorPicker.colors.hexString = this.value;
 });
+
+colorPicker.on(["mount", "color:setActive", "color:change"], function(){
+  // colorPicker.color is always the active color
+  const index = colorPicker.colors.index;
+  const hexString = colorPicker.colors.hexString;
+  activeColor.innerHTML = `
+    <div class="swatch" style="background: ${ hexString }"></div>
+    <span>${ index }: ${ hexString }</span>
+  `;
+})
