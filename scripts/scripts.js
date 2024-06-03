4594a74de78a5995abbbcce98e6f6600a37e19e9
EslamAbdElhakem <eslam.hakem12@gmail.com>
2024-06-03 12:50:00 +0300

    Refactor color picker code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,16 @@
-const hexCode = document.querySelector("#hex-code");
-const rgbCode = document.querySelector("#rgb-code");
-const hexInput = document.querySelector("#hexInput");
-const colorPreview = document.querySelector("#color-preview-box");
-
+/* create a new color picker instance */
 const colorPicker = new iro.ColorPicker("#picker", {
   width: 150,
   color: "#f00"
 });
 
+/* get the color picker's color */
+const hexCode = document.querySelector("#hex-code");
+const rgbCode = document.querySelector("#rgb-code");
+const hexInput = document.querySelector("#hexInput");
+const colorPreview = document.querySelector("#color-preview-box");
+
+/* set the color picker's color */
 colorPicker.on(["color:init", "color:change"], function 
 (color) {
   hexCode.innerHTML = ("HEX Code: " + color.hexString);
@@ -16,6 +19,7 @@ colorPicker.on(["color:init", "color:change"], function
   colorPreview.style.backgroundColor = color.hexString;
 });
 
+/* change the color picker's color */
 hexInput.addEventListener('change', function
 (color) {
   colorPicker.color.hexString = this.value;
